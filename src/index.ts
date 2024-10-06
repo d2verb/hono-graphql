543afdfe50c7f357fe6f6d66b9ee266364119205
d2verb <licprog73@gmail.com>
2024-10-06 18:16:07 +0900

    add graphql starter code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import { createYoga } from "graphql-yoga";
 import { Hono } from "hono";
-
+import { schema } from "./schema";
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+const yoga = createYoga({
+  graphqlEndpoint: "/",
+  schema,
 });
+
+app.mount("/graphql", yoga);
 
 export default app;
