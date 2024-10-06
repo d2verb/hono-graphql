d0fb4954002c8f5bfe2bfb3f6085bbb6d135e2a4
d2verb <licprog73@gmail.com>
2024-10-06 20:40:08 +0900

    add getPostsByUserId skelton

--- src/schema.ts
+++ src/schema.ts
@@ -7,6 +7,38 @@ builder.queryType({
     hello: t.string({
       resolve: () => "world",
     }),
+
+    getPostsByUserId: t.field({
+      type: [Post],
+      args: {
+        userId: t.arg.string(),
+      },
+      resolve: async (_, { userId }) => {
+        return [
+          {
+            id: "1",
+            title: "test",
+            content: "test",
+          },
+        ];
+      },
+    }),
+  }),
+});
+
+interface Post {
+  id: string;
+  title: string;
+  content: string;
+}
+
+const Post = builder.objectRef<Post>("Post");
+
+Post.implement({
+  fields: (t) => ({
+    id: t.exposeString("id"),
+    title: t.exposeString("title"),
+    content: t.exposeString("content"),
   }),
 });
 
