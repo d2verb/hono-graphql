471307ef1369037468d4b38df6c17a68fe9803f9
d2verb <licprog73@gmail.com>
2024-10-12 03:43:15 +0900

    use faker to create dummy data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,25 @@
+import { faker } from "@faker-js/faker";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.create({
-    data: {
-      email: "test@test.com",
-      name: "Test User",
-    },
+  const users = await prisma.user.createManyAndReturn({
+    data: [...Array(10)].map(() => ({
+      email: faker.internet.exampleEmail(),
+      name: faker.person.fullName(),
+    })),
   });
 
-  await prisma.post.create({
-    data: {
-      title: "Test Post",
-      content: "Test Content",
-      userId: user.id,
-    },
-  });
+  for (const user of users) {
+    await prisma.post.createMany({
+      data: [...Array(10)].map(() => ({
+        title: faker.lorem.sentence(),
+        content: faker.lorem.paragraph(),
+        userId: user.id,
+      })),
+    });
+  }
 }
 
 main()
